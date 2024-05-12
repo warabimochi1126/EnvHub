b462db3a25e8504ebbd49e73d32917503c1e8c97
warabimochi <satomi920003@yahoo.co.jp>
2024-05-13 00:23:03 +0900

    feat:#58 免責事項をモーダルウィンドウを使って描画する

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,14 @@
-import Link from "next/link";
+import { Disclaimer } from "./components/disclaimer";
 import MainCard from "./components/main-card";
-import Image from "next/image";
 
 export default function Home() {
   return (
     <>
-      {/* <Link href="/">
-        <Image src="/top-icon.png" alt="アイコン" width={50} height={50} className="absolute top-5 left-5" />
-      </Link> */}
       <div className="h-screen flex justify-evenly items-center">
         <MainCard text="envを共有する" redirectTo={`${process.env.SITE_DOMAIN}/share/post`} />
         <MainCard text="envを共有してもらう" redirectTo={`${process.env.SITE_DOMAIN}/share/get`} />
       </div>
+      <Disclaimer />
     </>
   );
 }
