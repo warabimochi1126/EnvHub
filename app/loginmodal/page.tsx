ddb3d1b0c0ff189b277afd08379c85fbc82dfb76
warabimochi <satomi920003@yahoo.co.jp>
2024-09-01 00:12:10 +0900

    feat:#67 ログイン画面を代替するモーダルを作成する

--- app/loginmodal/page.tsx
+++ app/loginmodal/page.tsx
@@ -3,6 +3,7 @@
 import { useState } from "react";
 import { FiGithub } from "react-icons/fi";
 import { IoMdClose } from "react-icons/io";
+import { IoLogoGithub } from "react-icons/io5";
 
 import Modal from "react-modal";
 
@@ -24,7 +25,7 @@ const modalStyle = {
     borderRadius: "1rem",
     padding: "1.5rem",
     width: "400px",
-    height: "200px",
+    // height: "200px",
   },
 };
 
@@ -34,17 +35,31 @@ export default function LoginModal() {
   return (
     <div className="App">
       <div onClick={() => setIsOpen(true)}>モーダルを表示する</div>
-      <Modal isOpen={modalIsOpen} style={modalStyle}>
+      <Modal
+        isOpen={modalIsOpen}
+        style={modalStyle}
+        onRequestClose={() => setIsOpen(false)}
+        closeTimeoutMS={100}
+      >
         <div className="flex justify-between">
-          <div className="flex">
-            <FiGithub size={22} className="mr-2" />
-            <span className="font-bold">GitHub Login</span>
+          <div className="flex items-center">
+            <FiGithub size={22} className="mr-1" />
+            <span className="text-lg font-bold">GitHub Login</span>
           </div>
-          <IoMdClose size={22} />
+          <IoMdClose
+            size={34}
+            onClick={() => setIsOpen(false)}
+            className="cursor-pointer rounded-full hover:bg-gray-200 p-1.5 transition-colors duration-200"
+          />
         </div>
-        <button className="w-full mt-3 flex justify-center items-center">
-          <FiGithub className="mr-1" />
-          GitHubでログイン
+        <div>
+          <div className="text-gray-500 text-sm">
+            <p>EnvHubの利用にはGitHubアカウントでのログインが必要です。</p>
+          </div>
+        </div>
+        <button className="w-full mt-3 flex justify-center items-center bg-gray-900 rounded text-white py-2 transition duration-200 ease-in-out hover:bg-gray-800 hover:shadow-md">
+          <IoLogoGithub className="mr-2" size={20} />
+          <span>GitHubでログイン</span>
         </button>
       </Modal>
     </div>
