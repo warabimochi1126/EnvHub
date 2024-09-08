f64a0b2be9ea19c5a8dd45cdb08e9c3f8302fb6c
warabimochi <satomi920003@yahoo.co.jp>
2024-09-08 21:03:33 +0900

    feat:ログインモーダルを綺麗なデザインに変更する

--- app/loginmodal/page.tsx
+++ app/loginmodal/page.tsx
@@ -31,37 +31,52 @@ const modalStyle = {
 
 export default function LoginModal() {
   const [modalIsOpen, setIsOpen] = useState<boolean>(false);
+  const modalOpen = () => setIsOpen(true);
+  const modalClose = () => setIsOpen(false);
 
   return (
     <div className="App">
-      <div onClick={() => setIsOpen(true)}>モーダルを表示する</div>
+      <div onClick={modalOpen}>モーダルを表示する</div>
       <Modal
         isOpen={modalIsOpen}
         style={modalStyle}
-        onRequestClose={() => setIsOpen(false)}
+        onRequestClose={modalClose}
         closeTimeoutMS={100}
       >
-        <div className="flex justify-between">
-          <div className="flex items-center">
-            <FiGithub size={22} className="mr-1" />
-            <span className="text-lg font-bold">GitHub Login</span>
-          </div>
-          <IoMdClose
-            size={34}
-            onClick={() => setIsOpen(false)}
-            className="cursor-pointer rounded-full hover:bg-gray-200 p-1.5 transition-colors duration-200"
-          />
-        </div>
-        <div>
-          <div className="text-gray-500 text-sm">
-            <p>EnvHubの利用にはGitHubアカウントでのログインが必要です。</p>
-          </div>
-        </div>
-        <button className="w-full mt-3 flex justify-center items-center bg-gray-900 rounded text-white py-2 transition duration-200 ease-in-out hover:bg-gray-800 hover:shadow-md">
-          <IoLogoGithub className="mr-2" size={20} />
-          <span>GitHubでログイン</span>
-        </button>
+        <InModalContent modalCloseFunc={modalClose} />
       </Modal>
     </div>
+  );
+}
+
+interface InModalContent {
+  modalCloseFunc: () => void;
+}
+
+export function InModalContent({ modalCloseFunc }: InModalContent) {
+  return (
+    <>
+      <div className="flex justify-between">
+        <div className="flex items-center">
+          <FiGithub size={22} className="mr-1" />
+          <span className="text-lg font-bold">GitHub Login</span>
+        </div>
+        <IoMdClose
+          size={34}
+          onClick={modalCloseFunc}
+          className="cursor-pointer rounded-full hover:bg-gray-200 p-1.5 transition-colors duration-200 relative -top-3 left-3"
+        />
+      </div>
+      <div>
+        <div className="text-gray-500 mt-2">
+          <p>EnvHubの利用にはGitHubアカウントでのログインが必要です。</p>
+        </div>
+      </div>
+      <hr className="h-2 mt-2" />
+      <button className="w-full mt-1 flex justify-center items-center bg-gray-900 rounded-lg text-white py-2 transition-colors duration-300 hover:bg-gray-800 hover:shadow-md">
+        <IoLogoGithub className="mr-2" size={20} />
+        <span>GitHubでログイン</span>
+      </button>
+    </>
   );
 }
