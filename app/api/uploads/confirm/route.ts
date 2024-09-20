056f04079d74218d746ca061007dc25ccbe692cf
warabimochi <satomi920003@yahoo.co.jp>
2024-09-20 12:37:22 +0900

    metadataのみ取得できるようにする

--- app/api/uploads/confirm/route.ts
+++ app/api/uploads/confirm/route.ts
@@ -1,8 +1,10 @@
+import { createClient } from "@/lib/supabase/server";
 import { NextRequest } from "next/server";
 
+const supabase = createClient();
 export async function POST(request: NextRequest) {
   const receivedFormData = await request.formData();
-  console.log(receivedFormData);
+  console.log(receivedFormData.getAll("metaData"));
 
   return Response.json({ aaa: "bbb" });
 }
