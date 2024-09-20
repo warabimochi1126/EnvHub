4d1fb2fcebb4e57688cb160a131b8d18dd306e22
warabimochi <ecccompsenyou@gmail.com>
2024-09-20 15:21:32 +0900

    エンドポイント側で受け取れるようにする

--- app/api/uploads/confirm/route.ts
+++ app/api/uploads/confirm/route.ts
@@ -1,10 +1,43 @@
 import { createClient } from "@/lib/supabase/server";
 import { NextRequest } from "next/server";
 
-const supabase = createClient();
-export async function POST(request: NextRequest) {
-  const receivedFormData = await request.formData();
-  console.log(receivedFormData.getAll("metaData"));
+interface MetaData {
+  repo_name: string;
+  repo_id: number;
+}
 
-  return Response.json({ aaa: "bbb" });
+export async function POST(request: NextRequest) {
+  // const supabase = createClient();
+
+  try {
+    const receivedFormData = await request.formData();
+    const metaDataInFormData = receivedFormData.get("meta_data");
+    const metaData = JSON.parse(metaDataInFormData as string);
+    // prettier-ignore
+    const uploadTargetFiles = receivedFormData.getAll("upload_target_files") as File[];
+
+    // TODO: バリデーションの実装
+
+    console.log(metaData);
+    console.log(metaData.repo_id);
+    console.log(metaData.repo_name);
+
+    console.log(uploadTargetFiles[0]);
+
+    // TODO: 紐づけリポジトリを本人が保持しているかの確認
+
+    // TODO: アップロードロジックの実装
+    // 1.DBのリポジトリテーブルにリポジトリ名・リポジトリID・コミットハッシュみたいな物のIDをアップサートする
+    // 2.コミットハッシュテーブルにコミットハッシュID・そのタイミングでアップロードされたファイル全てのIDをインサートする
+  } catch (e) {
+    return Response.json(
+      { message: "何らかの理由でファイルアップロードが成功しませんでした。" },
+      { status: 500 }
+    );
+  }
+
+  return Response.json(
+    { message: "ファイルアップロードに成功しました！" },
+    { status: 200 }
+  );
 }
---
+++ validation/backend/file-upload-validation.ts
@@ -0,0 +1,4 @@
+// TODO:便宜的にバリデーションをbackendディレクトリに入れているが、FEでも使いそうなら共通化する
+import { z } from "zod";
+
+function isEnvFiles(uploadTargetFiles: File[]) {}
