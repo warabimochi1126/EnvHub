4b4d15a582fd7b22210c5e335c1b8e89ea487641
warabimochi1126 <54062162+warabimochi1126@users.noreply.github.com>
2024-09-23 16:41:57 +0900

    feat(getpage): コミット履歴・コミットに紐づいたストレージを取得出来るAPIの作成 (#146)
    * feat(getpage): getページ側のAPIを作成する
    
    * feat(getpage): コミット履歴・コミットに紐づいたストレージを取得出来るAPIの作成
    
    * feat(getpage): コミット履歴・コミットに紐づいたストレージを取得出来るAPIの作成

---
+++ app/api/repositories/[repository_id]/commits/[commit_uuid]/files/route.ts
@@ -0,0 +1,65 @@
+import { isAuthUserRepository } from "@/datas/fetchAuthUserRespositories";
+import { createClient } from "@/lib/supabase/server";
+import { NextRequest } from "next/server";
+
+// TODO:後にservice,repositoryで分離する
+export async function GET(
+  request: NextRequest,
+  { params }: { params: { repository_id: string; commit_uuid: string } }
+) {
+  try {
+    const supabase = createClient();
+    const { repository_id: repositoryId, commit_uuid: commitUuid } = params;
+
+    // TODO:repoId: string,  commitId: stringでバリデーションする
+
+    // 紐づけリポジトリを本人が保持しているかの確認
+    const isAuthorizedRepository = isAuthUserRepository(Number(repositoryId));
+    if (!isAuthorizedRepository) {
+      return Response.json(
+        { message: "認証ユーザのが保持していないリポジトリを対象にファイルアップロードは行えません。" },
+        { status: 403 }
+      );
+    }
+
+    let targetCommitUuid: string = "";
+    if (commitUuid === "latest") {
+      const { data: latestCommitData, error: latestCommitError } = await supabase
+        .from("repository_latest_commits")
+        .select("latest_commit_uuid")
+        .eq("repository_id", repositoryId)
+        .single();
+
+      if (latestCommitData === null || latestCommitError) {
+        throw new Error();
+      }
+
+      targetCommitUuid = latestCommitData.latest_commit_uuid;
+    }
+
+    targetCommitUuid = targetCommitUuid ? targetCommitUuid : commitUuid;
+    // TODO:取得処理は正しいが、保存上手くいってなさそうなので、保存側を修正する
+    const { data: fileNames, error: commitFilesHistoryError } = await supabase
+      .from("commit_files_history")
+      .select("file_names")
+      .eq("commit_uuid", targetCommitUuid)
+      .single();
+
+    if (fileNames === null || commitFilesHistoryError) {
+      throw new Error();
+    }
+
+    const { data: storageData, error: storageError } = await supabase.storage
+      .from("envHub_storage")
+      .list(`${repositoryId}/${targetCommitUuid}`);
+
+    if (storageError) {
+      throw new Error();
+    }
+
+    // TODO:返すデータを精査する
+    return Response.json(storageData, { status: 200 });
+  } catch {
+    return Response.json({ message: "何らかの理由でファイル取得が成功しませんでした。" }, { status: 500 });
+  }
+}
---
+++ app/api/repositories/[repository_id]/commits/route.ts
@@ -0,0 +1,35 @@
+//
+
+import { isAuthUserRepository } from "@/datas/fetchAuthUserRespositories";
+import { createClient } from "@/lib/supabase/server";
+import { NextRequest } from "next/server";
+
+export async function GET(request: NextRequest, { params }: { params: { repository_id: string } }) {
+  try {
+    const supabase = createClient();
+    const repositoryId = params.repository_id;
+
+    // 紐づけリポジトリを本人が保持しているかの確認
+    const isAuthorizedRepository = isAuthUserRepository(Number(repositoryId));
+    if (!isAuthorizedRepository) {
+      return Response.json(
+        { message: "認証ユーザのが保持していないリポジトリを対象にファイルアップロードは行えません。" },
+        { status: 403 }
+      );
+    }
+
+    // repositoryIdに紐づいたcommitの情報を返す
+    const { data: commitList, error } = await supabase
+      .from("commit_files_history")
+      .select("commit_message, created_at")
+      .eq("parent_repository_id", repositoryId);
+
+    if (commitList === null || error) {
+      throw new Error();
+    }
+
+    return Response.json({ commit_list: commitList }, { status: 200 });
+  } catch {
+    return Response.json({ message: "コミット一覧が取得出来ませんでした。" }, { status: 500 });
+  }
+}
